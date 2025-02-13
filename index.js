17cd7741ce800a140f861900d44e20d672152a4c
JoMaCoBa <correabajo@gmail.com>
2025-02-13 12:00:02 -0600

    Configuration of cors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require ('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
 const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 routerApi(app);
 
