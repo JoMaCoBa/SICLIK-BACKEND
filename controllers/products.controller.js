f1a59e119decf90c62515c15034af1463867e601
JoMaCoBa <correabajo@gmail.com>
2025-02-13 13:17:00 -0600

    Added enpoint to obtain details of product

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -9,3 +9,28 @@ exports.getAllProducts = async (req, res) => {
   }
 };
 
+exports.getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error to obtain product" });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    console.log("Body recibido:", JSON.stringify(req.body, null, 2));;
+    const { name, price, image, description } = req.body;
+    const product = await Product.create({name, price, image, description});
+    res.status(201).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error to create product" });
+  }
+};
--- routes/products.router.js
+++ routes/products.router.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const ProductsController = require('../controllers/products.controller');
 
 router.get('/', ProductsController.getAllProducts);
+router.get('/:id', ProductsController.getProduct);
+router.post('/', ProductsController.createProduct);
 
 module.exports = router;
