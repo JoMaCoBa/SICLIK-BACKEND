ab05ba486c4cc9525dc6366d55c2dbe24cafa5aa
JoMaCoBa <correabajo@gmail.com>
2025-02-12 19:23:10 -0600

    Install and Configuration of Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Running in port: ${port}`);
+});
+
+app.get('/', (request, response) => {
+  response.send("Hello, from my server in express");
+})
